19b740ef68731d3f53a74f345017afdea9fdae44
gabriel-dtm <gabriel.almeida-dtm@esgas.com.br>
2022-08-24 08:36:14 -0300

    Adding error catcher

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
+import { AppError } from "./Error/appError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -13,6 +15,17 @@ app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(400).json({ message: err.message });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () =>
   console.log("Server is running on http://localhost:3333")
